af835b6bd602d5bfc1e72f7c9ec531f3180fe652
Konstantin Marinov <konstantin.marinov17@gmail.com>
2026-07-05 11:59:43 +0300

    Extracted a navigation component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { PageNavigation } from './page-navigation/page-navigation';
 
 @Component({
   selector: 'app-root',
+  imports: [PageNavigation],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/page-navigation/page-navigation.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-navigation',
+  templateUrl: './page-navigation.html',
+  styleUrl: './page-navigation.scss'
+})
+export class PageNavigation {}
