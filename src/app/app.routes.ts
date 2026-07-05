17722c68853d0c5f01b005d3a94945fc0d41d557
Konstantin Marinov <konstantin.marinov17@gmail.com>
2026-07-05 12:12:53 +0300

    Added a portfolio page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PortfolioPage } from './page/portfolio-page/portfolio-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'portfolio'
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioPage
+  }
+];
---
+++ src/app/page/portfolio-page/portfolio-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-page',
+  templateUrl: './portfolio-page.html',
+  styleUrl: './portfolio-page.scss'
+})
+export class PortfolioPage {}
