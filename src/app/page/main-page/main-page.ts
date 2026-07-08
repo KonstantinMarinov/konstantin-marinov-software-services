e159c3a0817a38311e2bb25df3b9c20d241796d7
Konstantin Marinov <konstantin.marinov17@gmail.com>
2026-07-08 07:23:22 +0300

    Added button on main page

--- src/app/page/main-page/main-page.ts
+++ src/app/page/main-page/main-page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
+  imports: [RouterLink],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
